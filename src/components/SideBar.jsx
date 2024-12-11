73cb2acb5387272005478c0feb1cce7b8f87d245
jitendragonukkad <jitendra.vishwakarma@gonukkad.com>
2024-12-11 09:57:37 +0530

    change responsive section

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -21,7 +21,7 @@ const SideBar = ({sidebartoggle}) => {
                 </div>
             </div>
             {
-                sidebartoggle && <div className={` bg-gray-600  py-2 px-4 pb-10 md:flex fixed h-[92%] z-50   flex-col justify-between items-center `}>
+                sidebartoggle && <div className={` bg-gray-600    mt-1 px-4 pb-10 md:flex fixed h-[92%]  z-50   flex-col justify-between items-center `}>
                     <div className=' w-full my-4 h-full flex flex-col justify-between'>
                         <ul className=' space-y-2   *:text-2xl w-full  *:py-2 *:items-center *:flex *:flex-col text-white  *:cursor-pointer *:border-l-4  *:border-l-transparent'>
                             <li className='hover:border-blue-500 hover:text-blue-500 '><IoHomeOutline /></li>
